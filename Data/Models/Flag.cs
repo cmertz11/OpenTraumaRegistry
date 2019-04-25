0ec8854a46d5ba1f156b66c5b2072004d9eeeedd
cmertz11 <cmertz11@gmail.com>
2019-04-24 20:03:39 -0400

    Begin coding Flag system models.

---
+++ Data/Models/Flag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TraumaData.Models
+{
+    public class Flag
+    {
+        public int Id { get; set; }
+        public string Note { get; set; }
+        public bool IsActive { get; set; }
+        //FK
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        public int FlagTypeId { get; set; }
+
+        public List<FlagReminder> Reminders { get; set; }
+
+    }
+}
---
+++ Data/Models/FlagReminder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TraumaData.Models
+{
+    public class FlagReminder
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string NotifyEmailList { get; set; }
+        public string CronTab { get; set; }
+
+    }
+}
