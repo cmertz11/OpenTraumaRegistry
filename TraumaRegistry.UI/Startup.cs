16b99315669b5a729cbefdb048527279a0f59e16
Chris <cmertz11@gmail.com>
2019-10-09 17:18:18 -0400

    Continued work on modal add/edit form for reftable modification.

--- TraumaRegistry.UI/Startup.cs
+++ TraumaRegistry.UI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazored.Modal;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -27,6 +28,7 @@ namespace TraumaRegistry.UI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddBlazoredModal();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
