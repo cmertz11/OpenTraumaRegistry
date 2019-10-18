dc6e3b3dedd5cfcf75c077bb1c79de8ce7de9fdb
Chris <cmertz11@gmail.com>
2019-10-18 08:01:59 -0400

    Working EditRefTableRecord.

--- TraumaRegistry.Api/Controllers/ReferenceTableController.cs
+++ TraumaRegistry.Api/Controllers/ReferenceTableController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Dynamic.Core;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using TraumaRegistry.Data;
 using TraumaRegistry.Data.Models;
@@ -36,7 +37,7 @@ namespace TraumaRegistry.Api.Controllers
                 {
                     while( result.Read())
                     {
-                        table.tableData.Add(new refTableDTO.refTable { 
+                         table.tableData.Add(new refTableDTO.refTable { 
                             Id = Convert.ToInt32(result["Id"]), 
                             Code = result["Code"].ToString(), 
                             Description = result["Description"].ToString() });          
@@ -52,7 +53,7 @@ namespace TraumaRegistry.Api.Controllers
         {
             string sql = string.Format("SELECT name FROM Sys.Tables where name LIKE 'Ref%'");
 
-                List<String> refTables = new List<string>();
+            List<String> refTables = new List<string>();
             using (var command = _context.Database.GetDbConnection().CreateCommand())
             {
                 command.CommandText = sql;
@@ -62,12 +63,56 @@ namespace TraumaRegistry.Api.Controllers
                     while (result.Read())
                     {
                         refTables.Add(result[0].ToString());
-                    }
-                        
-                 
+                    } 
                 }
             }
             return refTables;
+        }
+
+        [HttpPatch]
+        [ActionName("AddRefTableRecord")]
+        public int AddRefTableRecord(string tableName, refTableDTO.refTable newRec)
+        {
+            string sql = string.Format("INSERT INTO dbo.{0} (Code, Description) VALUES ('{1}', '{2}'); SELECT SCOPE_IDENTITY() AS Id;", tableName, newRec.Code, newRec.Description);
+            int NewId = 0;
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = sql;
+                _context.Database.OpenConnection();
+                using (var result = command.ExecuteReader())
+                {
+                    while (result.Read())
+                    {
+                        if(int.TryParse(result[0].ToString(), out NewId))
+                        {
+                            return NewId;
+                        }
+                    }
+                }
+                return NewId;
+            }
+        }
+
+        [HttpPatch]
+        [ActionName("EditRefTableRecord")]
+        public bool EditRefTableRecord(string tableName, refTableDTO.refTable updatedRec)
+        {
+            try
+            {
+                string sql = string.Format("UPDATE dbo.{0} ", tableName);
+                sql += "SET (Code = @Code, Description =  WHERE Id = @Description;";
+                var code = new SqlParameter("@Code", updatedRec.Code);
+                var description = new SqlParameter("@Description", updatedRec.Description);
+
+                _context.Database.ExecuteSqlRaw(sql, code, description);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+             
         }
 
         public class refTableDTO
